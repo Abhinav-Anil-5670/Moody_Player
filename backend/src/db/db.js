19f8ef601efb97d448bc5a4239481a19745f72e6
Abhinav Anil <abhinavanil800@gmail.com>
2025-09-23 23:05:50 +0530

    Started_Code

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 function connectToDB(){
     mongoose.connect(process.env.MONGODB_URL).then(()=>{
-        console.log("Connected to Database DB")
+        console.log("Connected to Database")
     })
     .catch((err)=>{
         console.log(err)
